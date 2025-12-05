8825522393db7dc6314837090a9909bd23064dc1
Salem Adams <salemadamscs@gmail.com>
2025-12-04 22:38:17 -0500

    feat: add socket.io client

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -1,6 +1,6 @@
 const ENV = {
   dev: {
-    apiUrl: 'http://localhost:4000',
+    apiUrl: 'http://10.20.56.38:4000',
   },
   prod: {
     apiUrl: process.env.EXPO_PUBLIC_API_URL || 'https://your-production-url.com',
---
+++ client/src/services/SocketIoClient.ts
@@ -0,0 +1,36 @@
+import EventEmitter from "react-native/Libraries/vendor/emitter/EventEmitter";
+import { io, Socket } from "socket.io-client";
+import API from "../config/api";
+export default class SocketIoClient extends EventEmitter {
+  socket?: Socket;
+  constructor() {
+    super();
+    this.socket = undefined;
+    this._connect();
+  }
+
+  private _connect() {
+    const options = {
+      autoConnect: true,
+      forceNew: false,
+      reconnection: true,
+      reconnectionAttempts: 3,
+      reconnectionDelay: 3000,
+      transports: ["polling", "websocket"],
+    };
+
+    this.socket = io(API.apiUrl, options);
+
+    this.socket.on("connection", () => {
+      this.emit("connection");
+    });
+
+    this.socket.on("disconnect", () => {
+      this.emit("disconnect");
+    });
+
+    this.socket.on("connect_error", (err: any) => {
+      console.log(`connect_error due to ${err.message}`);
+    });
+  }
+}
